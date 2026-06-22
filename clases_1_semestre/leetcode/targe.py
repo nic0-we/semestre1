bc25c95b878af75232253cfb76d4b79ea244d462
nic0-we <nicolasmontes977@gmail.com>
2026-06-22 16:10:11 +0000

    agrega archivo con un mal codgi (estudiar)

--- clases_1_semestre/leetcode/targe.py
+++ clases_1_semestre/leetcode/targe.py
@@ -1,9 +1,9 @@
-nums = [1,2,3,4,5]
+list = [1,2,3,4,5]
 
-target = int(input("Enter the target number: "))
-
-for i in range(len(nums)):
-    for j in range(i + 1, len(nums)):
-        if nums[i] + nums[j] == target:
-            print(f"Indices: {i}, {j}")
-            
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+target = 9
+print(twoSum(list, target))
